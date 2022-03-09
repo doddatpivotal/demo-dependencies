1b70d9b711551f74107b8c6624a2d0de34710a55
doddatpivotal <dpfeffer@vmware.com>
2022-03-09 14:35:36 -0500

    Debugging

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,9 +15,23 @@ pushd packages/minio/1.0.0
 vendir sync
 popd
 
-# mv packages/minio/1.0.0/bundle/chart/values.yaml packages/minio/1.0.0/bundle/chart/values.yaml.original
-# echo -e "#@data/values\n---\n\n" > packages/minio/1.0.0/bundle/chart/values.yaml
-# ytt -f packages/minio/1.0.0/bundle/chart/values.yaml.original --file-mark "values.yaml.original:type=yaml-plain" -f packages/minio/1.0.0/helm/values-overlay.yaml >> packages/minio/1.0.0/bundle/chart/values.yaml
+WORKING_FOLDER=packages/minio/1.0.0/bundle
+SERVICE_FOLDER=minio
+
+mv ${WORKING_FOLDER}/chart/values.yaml ${WORKING_FOLDER}/chart/values.yaml.original
+echo -e "#@data/values\n---\n\n" > ${WORKING_FOLDER}/chart/values.yaml
+ytt -f ${WORKING_FOLDER}/chart/values.yaml.original --file-mark "values.yaml.original:type=yaml-plain" -f ${WORKING_FOLDER}/../helm/values-overlay.yaml >> ${WORKING_FOLDER}/chart/values.yaml
+
+CHART_VERSION=$(yq eval .version ${WORKING_FOLDER}/chart/Chart.yaml)
+APP_VERSION=$(yq eval .appVersion ${WORKING_FOLDER}/chart/Chart.yaml)
+
+# Add chart version to the end of the values file.
+yq eval '{"chartInfo": { "appVersion": .appVersion } } ' ${WORKING_FOLDER}/chart/Chart.yaml >> ${WORKING_FOLDER}/chart/values.yaml
+
+echo -e "#! I am a sample that was automatically generated with ${BUILD_SCRIPT}\n\n---" > packages/minio/1.0.0/sample/${SERVICE_FOLDER}-${CHART_VERSION}.yaml
+helm template ${PACKAGE} ${WORKING_FOLDER}/chart --namespace sample-namespace --include-crds \
+| ytt --ignore-unknown-comments -f ${WORKING_FOLDER}/chart/values.yaml -f - -f ${WORKING_FOLDER}/config \
+| kbld -f - --imgpkg-lock-output ${WORKING_FOLDER}/.imgpkg/images.yml >> packages/minio/1.0.0/sample/${SERVICE_FOLDER}-${CHART_VERSION}.yaml
 
 # Generate package file from from template
 ytt -f packages/minio/1.0.0/package-template.yaml \
